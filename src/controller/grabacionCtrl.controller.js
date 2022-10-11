b56a7ab85db5f93fe1306d8a537248633a40d7ed
moferrin <moferrin@espe.edu.ec>
2022-10-11 16:36:36 -0500

    controlador -> mongodb

--- src/controller/grabacionCtrl.controller.js
+++ src/controller/grabacionCtrl.controller.js
@@ -1,16 +1,28 @@
 import Grabacion from '../models/Grabacion.model'
 
 export const todasGrabaciones = async (req, res) => {
-    //const grabaciones = await Grabacion.find();
-    res.send('hola get');
+    const grabaciones = await Grabacion.find();
+    res.json(grabaciones);
 }
 
 export const crearGrabacion = async (req, res) => {
-    const grabaciones = await Grabacion.find();
-    res.send('hola post');
+    const grabacion = new Grabacion({
+        lenguaGrab: req.body.lenguaGrab,
+        lenguaMadre: req.body.lenguaMadre,
+        lenguaSecundaria: req.body.lenguaSecundaria,
+        ciudad: req.body.ciudad,
+        nota: req.body.nota,
+        apellidoNombre: req.body.apellidoNombre,
+        edad: req.body.edad,
+        genero: req.body.genero,
+    })
+
+    const grabacionGuard = await grabacion.save();
+    res.send(grabacionGuard);
 }
 
 export const actualizarGrabacion = async (req, res) => {
-    const grabaciones = await Grabacion.find();
-    res.send('hola put');
+    const id = req.params;
+    const grabacionActu = await Grabacion.findByIdAndUpdate(id,{$set: req.body}, {new:true})
+    res.send(grabacionActu);
 }
