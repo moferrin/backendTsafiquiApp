778cefb8e6192f902cc056402a12c97f8f06aa0e
moferrin <moferrin@espe.edu.ec>
2022-10-22 14:30:09 -0500

    body-parser

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+const bodyParser = require('body-parser'); 
 //import IndexRoutes from './routes/index'
 import GrabacionRoute from './routes/grabacion.routes';
 
 const app = express();
-
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 app.use('/grabacion',GrabacionRoute);
