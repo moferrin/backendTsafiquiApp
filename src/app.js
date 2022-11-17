4460265ad7f523fc3cd18b46a4468a845f53ab64
moferrin <moferrin@espe.edu.ec>
2022-11-16 23:07:05 -0500

    archivos ruta y  libreria

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import express from 'express';
-const bodyParser = require('body-parser'); 
-//import IndexRoutes from './routes/index'
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload')
+
+
 import GrabacionRoute from './routes/grabacion.routes';
+import UploadRoute from './routes/uploads.routes';
 
 const app = express();
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
+app.use(fileUpload())
 app.use('/grabacion',GrabacionRoute);
-//app.use(IndexRoutes);
+app.use('/upload',UploadRoute);
 
 export default app;
--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-//import {Router} from 'express'
-//const router = Router()
-//import router from './grabacion.routes'
-/*
-router.get('/grabacion', (req, res) =>{
-    res.send('hola');
-});*/
-
-//export default router;
